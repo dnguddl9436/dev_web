95cc02c715fcf5c58a8db8f9604dd7c8f9a13265
dnguddl9436 <dnguddl9696@gmail.com>
2021-04-01 21:20:05 +0900

    210401

---
+++ dev_web/src/web/basic/HelloWeb.java
@@ -0,0 +1,19 @@
+package web.basic;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/hello")
+public class HelloWeb extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse res) 
+			throws ServletException, IOException {
+		PrintWriter out = res.getWriter();
+		out.println("<b>Hello Web Service!!!</b>");
+	}
+}
