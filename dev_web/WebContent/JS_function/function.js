87a159e2928417b522b639e52f425e3ec04b1577
dnguddl9436 <dnguddl9696@gmail.com>
2021-04-26 22:17:01 +0900

    210426

---
+++ dev_web/WebContent/JS_function/function.js
@@ -0,0 +1,9 @@
+// function
+// 1. Function Declation
+// - one function === one thing
+// - function name(param1, param2) { 실행부...... return; }
+// - function is object in JS - 프로토타입
+function test() {
+  console.log("hello world!");
+}
+test();
